3aab1897a30c877757bb3308eebaa93c3b7a18c3
Seshadri Mahalingam <seshadri@fb.com>
2011-07-24 01:24:02 -0700

    Summary:
    Task ID: #
    
    Blame Rev:
    
    Reviewers:
    
    CC:
    
    Test Plan:
    
    Revert Plan:
    
    Tags:
    
    - begin *PUBLIC* platform impact section -
    Bugzilla: #
    - end platform impact -

---
+++ tests/testprim.py
@@ -0,0 +1,16 @@
+import unittest
+
+import prim
+
+class PrimitiveSuccessTestCase(unittest.TestCase):
+
+    def testPlus(self):
+        self.assertEquals(prim.plus(1, 2, 3), 6)
+
+class PrimitiveFailureTestCase(unittest.TestCase):
+
+    def testPlus(self):
+        self.assertRaises(TypeError, prim.plus, 1, "two", 3)
+
+# import sys
+# unittest.main(sys.modules[__name__])
