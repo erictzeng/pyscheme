309a9706919158a9ef2bed5b922fba436bd82a6b
Seshadri Mahalingam <seshadri@fb.com>
2011-07-23 23:08:16 -0700

    [data] print cons pairs
    Summary:
    Print cons pairs in Scheme fashion. Print a (cons a b) as (a . b) unless b is
    also a cons pair in which case (a b) is used (b lacks parens here).
    
    Reviewers: erictzeng, cjcai
    
    Test Plan:
    $ python
    >>> from data import ConsPair as cons
    >>> print cons(1, 2), cons(1, cons(2, 3)), cons(1, cons(2, None))
    
    Conflicts:
    
    	data.py

--- data.py
+++ data.py
@@ -60,6 +60,22 @@ class ConsPair(object):
         self.car = car
         self.cdr = cdr
 
+    def __repr_no_parens__(self):
+        if self.cdr == None:
+            return repr(self.car)
+        elif isinstance(self.cdr, ConsPair):
+            return '%s %s' % (repr(self.car), self.cdr.__repr_no_parens__())
+        else:
+            return '%s . %s' % (repr(self.car), repr(self.cdr))
+
+    def __repr__(self):
+        if self.cdr == None:
+            return repr(self.car)
+        elif isinstance(self.cdr, ConsPair):
+            return '(%s %s)' % (repr(self.car), self.cdr.__repr_no_parens__())
+        else:
+            return '(%s . %s)' % (repr(self.car), repr(self.cdr))
+
     def __str__(self):
         cdr = str(self.cdr)
         if cdr[0] == "(" and cdr[-1] == ")":
@@ -80,7 +96,6 @@ class ConsPair(object):
 class Nil(object):
     def __str__(self):
         return "()"
-
 
 class Vector(object):
     
