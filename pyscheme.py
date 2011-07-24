764e69bc90e4f38464136f38240ff62514eacfe8
Dan Wang <d.wang1991@gmail.com>
2011-07-24 03:15:15 -0700

    Made tokenize_list and make_list functions to convert a string to a ConsPair list. note that this makes a list of lists, so multiple scheme functions can be called:
    (+ 5 3) will be interpreted as ((+ 5 3)) so that
    (+ 5 3)(- 2 5) can be interpreted as ((+ 5 3) (- 2 5))
    
    This makes it kind of ugly for single function calls, but it makes
    the recursion easier and doesn't require stripping parens before
    converting to tokens. I forget if scheme allows for multiple function
    calls in one line.
    
    --dan

--- pyscheme.py
+++ pyscheme.py
@@ -2,9 +2,58 @@ import ast
 import data
 import env
 import prim
+import re
 
 def tokenize(s):
     return s.replace('(', ' ( ').replace(')', ' ) ').split()
+
+def make_list(input):
+    # Input: string
+    tokenized = tokenize_list(input)
+    expression_list = data.Nil()
+    for token in reversed(tokenized):
+        if isinstance(token, str):
+            expression_list = data.ConsPair(make_list(token[1:-1]), expression_list)
+        else:
+            expression_list = data.ConsPair(token, expression_list)
+    return expression_list
+
+def tokenize_list(input):
+    # Returns a list of tokens, leaving parenthesized expressions as is
+    result = []
+    while input:
+        whitespace = re.match(r" +", input)
+        integer = re.match(r"\d+", input)
+        variable = re.match(r"[!$%&*+-./:<=>?@^_~a-zA-Z]([!$%&*+-./:<=>?@^_~a-zA-Z0-9])*", input)
+        # Removes starting whitespace
+        if whitespace:
+            input = input[len(whitespace.group()):]
+            continue
+        # Matches integers
+        elif integer:
+            result += [ast.IntLiteral(int(integer.group()))]
+            input = input[len(integer.group()):]
+            continue
+        # Matches variables
+        elif variable:
+            result += [ast.Identifier(variable.group())]
+            input = input[len(variable.group()):]
+        # Handles parenthesized expressions
+        elif input[0] == '(':
+            parencount = index = 1
+            while parencount > 0:
+                if input[index] == '(':
+                    parencount += 1
+                if input[index] == ')':
+                    parencount -= 1
+                index += 1
+            result += [input[:index]]
+            input = input[index:]
+            continue
+        else:
+            return None
+    return result
+
 
 # STUFF FOR TESTING, REMOVE LATER
 glob = env.GlobalEnv()
@@ -31,8 +80,7 @@ def read(tokens):
             return ast.Identifier(token)
 
 def repl(prompt = "pyscheme > "):
-    while True:
+    while not val == "(exit)":
         val = make_ast(raw_input(prompt)).eval(glob)
         print val.__repr__()
-
 repl()
