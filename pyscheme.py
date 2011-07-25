f06b5821c1a0fdfeaa5279ec26becdf0ee12ff34
Dan Wang <d.wang1991@gmail.com>
2011-07-24 19:03:26 -0700

    Summary: --------------------  * pyscheme.py    * Fixed some imports.
    --dan

--- pyscheme.py
+++ pyscheme.py
@@ -19,6 +19,8 @@ import data
 import env
 import prim
 import re
+import traceback
+import specialforms
 
 def tokenize(s):
     return s.replace('(', ' ( ').replace(')', ' ) ').split()
@@ -76,27 +78,6 @@ def tokenize_list(input_string):
 
 # STUFF FOR TESTING, REMOVE LATER
 glob = env.GlobalEnv()
-
-#def make_ast(s):
-#    tokens = tokenize(s)
-#    return read(tokens)
-
-#def read(tokens):
-#    token = tokens.pop(0)
-#    try:
-#        num = int(token)
-#        return ast.IntLiteral(num)
-#    except ValueError, TypeError:
-#        if token == "(":
-#            token_list = []
-#            while tokens[0] != ")":
-#                token_list.append(read(tokens))
-#            tokens.pop(0)
-#            return ast.ExpList(*token_list)
-#        elif token == ")":
-#            raise SyntaxError("mismatched parens")
-#        else:
-#            return ast.Identifier(token)
 
 def repl(prompt = "pyscheme > "):
     while True:
